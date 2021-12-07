90a1c9244bde9c41c5c25dcd94ff95ab2c6c6141
Bartek Sokorski <b.sokorski@gmail.com>
2021-12-07 22:41:22 +0100

    Day 7 - change from math.fabs to abs

--- solutions/day07/main.py
+++ solutions/day07/main.py
@@ -1,8 +1,7 @@
-import math
 
 with open("input.txt", "r") as input_file:
     crabs = list(map(int, input_file.read().split(",")))
 
 
-print(f"Part 1: {min([sum(list(map(lambda x: math.fabs(x-pos), crabs))) for pos in range(min(crabs), max(crabs)+1)])}")
-print(f"Part 2: {min([sum(list(map(lambda x: (math.fabs(x-pos) * (math.fabs(x-pos)+1) /2), crabs))) for pos in range(min(crabs), max(crabs)+1)])}")
+print(f"Part 1: {min([sum(list(map(lambda x: abs(x-pos), crabs))) for pos in range(min(crabs), max(crabs)+1)])}")
+print(f"Part 2: {min([sum(list(map(lambda x: (abs(x-pos) * (abs(x-pos)+1) /2), crabs))) for pos in range(min(crabs), max(crabs)+1)])}")
