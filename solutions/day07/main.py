1c9ce4a030cfaf6c7f6961b7f849a74ccac71957
Bartek Sokorski <b.sokorski@gmail.com>
2021-12-07 22:30:13 +0100

    Day 7 oneliners (because why tf not)

--- solutions/day07/main.py
+++ solutions/day07/main.py
@@ -4,20 +4,5 @@ with open("input.txt", "r") as input_file:
     crabs = list(map(int, input_file.read().split(",")))
 
 
-def solution(data):
-    best = float("inf")
-    best_2 = float("inf")
-
-    for pos in range(min(crabs), max(crabs)+1):
-        fuel = sum(list(map(lambda x: math.fabs(x-pos), crabs)))
-        fuel_2 = sum(list(map(lambda x: (math.fabs(x-pos) * (math.fabs(x-pos)+1) /2), crabs)))
-        if fuel < best:
-            best = fuel
-        if fuel_2 < best_2:
-            best_2 = fuel_2
-    return best, best_2
-
-part_1, part_2 = solution(crabs)
-
-print(f"Part 1: {part_1}")
-print(f"Part 2: {part_2}")
+print(f"Part 1: {min([sum(list(map(lambda x: math.fabs(x-pos), crabs))) for pos in range(min(crabs), max(crabs)+1)])}")
+print(f"Part 2: {min([sum(list(map(lambda x: (math.fabs(x-pos) * (math.fabs(x-pos)+1) /2), crabs))) for pos in range(min(crabs), max(crabs)+1)])}")
